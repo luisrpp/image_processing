319e4803e52e8397a4bd06066d71f4127088e778
Luis Roberto Pereira de Paula <luisrpp@gmail.com>
2017-08-23 00:26:13 -0300

    Hough Transform - image validation

--- lib/image_processing/hough_transform.rb
+++ lib/image_processing/hough_transform.rb
@@ -6,11 +6,18 @@ module ImageProcessing
     attr_reader :image
 
     def initialize(image)
+      validate_image(image)
       @image = image
     end
 
     def find_lines
       # TODO
+    end
+
+    private
+
+    def validate_image(image)
+      raise ImageProcessing::ImageProcessingError, 'Use a greyscale image!' unless image.bands == 1
     end
   end
 end
